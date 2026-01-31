2d28249b2e5c7fa7b6c48f6f19d50ce64c2f4182
NaMinhyeok <nmh9097@gmail.com>
2026-02-01 02:56:24 +0900

    FEAT: accesslog를 csv 파서를 이용하여 파싱한다 #3

--- core/src/main/java/io/github/naminhyeok/core/support/error/ErrorCode.java
+++ core/src/main/java/io/github/naminhyeok/core/support/error/ErrorCode.java
@@ -2,6 +2,9 @@ package io.github.naminhyeok.core.support.error;
 
 public enum ErrorCode {
 
-    E500
+    E500,
+
+    // 파싱 관련 에러 (1000번대)
+    E1000
 
 }
--- core/src/main/java/io/github/naminhyeok/core/support/error/ErrorType.java
+++ core/src/main/java/io/github/naminhyeok/core/support/error/ErrorType.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorType {
 
     DEFAULT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500, "An unexpected error has occurred.",
-        LogLevel.ERROR);
+        LogLevel.ERROR),
+
+    // 파싱 관련 에러 (1000번대)
+    PARSE_INVALID_REQUEST(HttpStatus.BAD_REQUEST, ErrorCode.E1000, "요청 데이터 형식이 올바르지 않습니다.",
+        LogLevel.INFO);
 
     private final HttpStatus status;
 
@@ -19,7 +23,6 @@ public enum ErrorType {
     private final LogLevel logLevel;
 
     ErrorType(HttpStatus status, ErrorCode code, String message, LogLevel logLevel) {
-
         this.status = status;
         this.code = code;
         this.message = message;

eddd1921b913abab3548c232c8288dd0766d1f0d
NaMinhyeok <nmh9097@gmail.com>
2026-02-01 12:37:15 +0900

    FEAT: LogAnalysisRepository에 findById 메서드 추가 #4
    - LogAnalysisRepository 인터페이스에 findById(Long) 정의
    - InMemoryLogAnalysisRepository에서 findById 구현
    - 존재하지 않는 ID 조회시 Optional.empty() 반환

--- core/src/main/java/io/github/naminhyeok/core/domain/LogAnalysisRepository.java
+++ core/src/main/java/io/github/naminhyeok/core/domain/LogAnalysisRepository.java
@@ -1,7 +1,11 @@
 package io.github.naminhyeok.core.domain;
 
+import java.util.Optional;
+
 public interface LogAnalysisRepository {
 
     LogAnalysis save(LogAnalysis logAnalysis);
+
+    Optional<LogAnalysis> findById(Long id);
 
 }
